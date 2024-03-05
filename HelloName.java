e5cdbd6b432d535451bcacefd24244ed5800c701
Andy Yu <162320855+Yundy05@users.noreply.github.com>
2024-03-05 12:45:41 +0000

    Add files via upload

---
+++ HelloName.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class HelloName {
+
+	public static void main(String[] args) {
+		System.out.print("What is your name? ");
+		Scanner input = new Scanner( System.in );
+		String name = input.next();
+		System.out.println("Hello " + name);
+	}
+
+}

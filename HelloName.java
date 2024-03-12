479f552ae1b85a7448a35e777469c466a80832d8
ChuanGitIt <zhaochuansailout@gmail.com>
2024-03-12 13:34:05 +0000

    ADDED

--- HelloName.java
+++ HelloName.java
@@ -4,6 +4,7 @@ public class HelloName {
 
 	public static void main(String[] args) {
 		System.out.print("What is your name? Again? Test Test Anothertest .....");
+		System.out.println("SBSBSBSBSBSBSBSBSB");
 		Scanner input = new Scanner( System.in );
 		String name = input.next();
 		for(int i =0; i<=999999999; i++)

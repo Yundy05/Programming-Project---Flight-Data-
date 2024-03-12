084d948ec54a91e3222b46791d54d680b703299f
VivianShong <shenho@tcd.ie>
2024-03-12 11:16:52 +0000

    Update HelloName.java
    hhhh

--- HelloName.java
+++ HelloName.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class HelloName {
 
 	public static void main(String[] args) {
-		System.out.print("What is your name? Again? Test Test Anothertest .....");
+		System.out.print("What is your name? Again? Test Test Anothertest ..... hhhh");
 		Scanner input = new Scanner( System.in );
 		String name = input.next();
 		for(int i =0; i<=999999999; i++)
